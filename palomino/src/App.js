c83caecd34c3670cc3076a9c7f2fecbc3e4e2439
Kyle Hill <kbhill@lakeheadu.ca>
2024-01-31 18:35:16 -0500

    added placeholder routes

--- palomino/src/App.js
+++ palomino/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" to element={<HomePage />} />
+          <Route path="/UserPage" element={<UserPage />} /> 
+          <Route path="/MatchPage" element={<MatchPage />} /> 
+          <Route page="/EditProfilePage" element={<EditProfilePage />} />
+          <Route page="/CreateAccountPage" element={<CreateAccountPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
